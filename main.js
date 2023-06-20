afae920debc15177d74c2a0523eaf4e93b8fc2c9
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2023-06-20 13:26:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function music1play(){
+    audio1.play();
+}
+
+function music2play(){
+    audio2.play();
+}
+
+function music3play(){
+    audio3.play();
+}
+
+function music4play(){
+    audio4.play();
+}
+
+function music5play(){
+    audio5.play();
+}
+
+function music6play(){
+    audio6.play();
+}
+
+function music7play(){
+    audio7.play();
+}
+
+function music8play(){
+    audio8.play();
+}
